fb60c430d1eb0b74bdd90a2a1cefe69910791e2e
Mnqobi Thusi <mnqobimvelas@gmail.com>
2024-11-04 02:20:41 +0200

    Respawn point set

--- Custom_JS/source.js
+++ Custom_JS/source.js
@@ -37,7 +37,15 @@ window.addEventListener('scroll', shadowHeader);
 /*================ EMAIL JS ================ */
 
 
-/*================ SHOW SCROLL UP ================ */
+// SHOW SCROLL UP
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class in the scroll-up class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 
 // PAGE ACTIVE LINK
